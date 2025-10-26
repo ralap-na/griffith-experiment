567b903baa244a4a46d61863b9e638866d6e51ca
ralap_na <lydia88na@gmail.com>
2025-10-26 22:31:07 +0800

    bug fixed: use HashRouter instead of BrowserRouter to avoid 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EnzymePage from "./pages/EnzymePage";
 import TransformationPage from "./pages/TransformationPage";
 
 export default function App() {
   return (
-      <Router basename="/griffith-experiment">
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/enzyme" element={<EnzymePage />} />
